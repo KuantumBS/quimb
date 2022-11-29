61a179fa850cb464cea47f7238360b09c5a19ed2
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2022-11-29 14:07:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     keywords='quantum physics tensor networks tensors dmrg tebd',
 )
